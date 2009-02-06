55294a54e0c511923fb95c865b1faa9688ddff53
Wilhelm Chung <wil@frogmetrics.com>
2009-02-06 11:47:48 -0500

    added vector class

---
+++ vector.lua
@@ -0,0 +1,80 @@
+--  A custom vector class
+
+Vector = {
+   identity = "This is a vector class",
+}
+
+function Vector:new(x, y)
+   local instance = {}
+   setmetatable(instance, self)
+   self.__index = self
+
+   instance.x = x
+   instance.y = y
+   return instance
+end
+
+function Vector:to_s()
+   return "(" .. self.x .. ", " .. self.y .. ")"
+end
+
+function Vector:is_nearby(threshold, a)
+   if a == self then
+      return false 
+   end
+   return self:distance(a) < threshold
+end
+
+function Vector:distance(a)
+   return math.sqrt((self.x - a.x)^2 + (self.y - a.y)^2)
+end
+
+function Vector:__add(a)
+   return Vector:new(self.x + a.x, self.y + a.y)
+end
+
+function Vector:__sub(a)
+   return Vector:new(self.x - a.x, self.y - a.y)
+end
+
+function Vector:__mul(num)
+   return Vector:new(self.x * num, self.y * num)
+end
+
+function Vector:__div(num)
+   if (num ~= 0) then
+      return Vector:new(self.x / num, self.y / num)
+   else
+      return self
+   end
+end
+
+function Vector:__unm()
+   return Vector:new(-self.x, -self.y)
+end
+
+function Vector:dot(a)
+   return self.x * a.x + self.y * a.y
+end
+
+function Vector:r()
+   return math.sqrt(self:dot(self))
+end
+
+
+-- a = Vector:new(2,2)
+-- b = Vector:new(5,6)
+-- print(a:to_s())
+-- print(b:to_s())
+-- print(a:distance(b))
+-- print(a:is_nearby(10, a))
+-- print(a:is_nearby(10, b))
+-- print(a:is_nearby(3, b))
+-- print((a+b):to_s())
+-- print((b-a):to_s())
+-- print((a*3):to_s())
+-- print((a/3):to_s())
+-- print((-a):to_s())
+-- print(a:dot(b))
+-- print(a:r())
+
