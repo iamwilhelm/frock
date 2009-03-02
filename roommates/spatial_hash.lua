bb31a09aa240ca5f0460f15279a28952773ce3da
Wilhelm Chung <wil@3cglabs.com>
2009-03-02 11:19:59 -0500

    bugfix: fixed incorrect spatial hashing function

--- roommates/spatial_hash.lua
+++ roommates/spatial_hash.lua
@@ -1,27 +1,27 @@
 -- returns a hash for the 2D spatial
 SpatialHash = {}
 
-function SpatialHash:new(xsize, ysize, resx, resy)
+function SpatialHash:new(xsize, ysize, xres, yres)
    local instance = {}
    setmetatable(instance, self)
    self.__index = self
 
    instance.xsize = xsize
    instance.ysize = ysize
-   instance.resx = resx
-   instance.resy = resy
+   instance.xres = xres
+   instance.yres = yres
 
    return instance
 end
 
 function SpatialHash:hash_x(x)
-   return math.floor((x / self.xsize) * self.resx)
+   return math.floor(x / (self.xsize / self.xres))
 end
 
 function SpatialHash:hash_y(y)
-   return math.floor((y / self.ysize) * self.resy)
+   return math.floor(y / (self.ysize / self.yres))
 end
 
 function SpatialHash:hash(x, y)
-   return self:hash_x(x) + self:hash_y(y) * self.resx
+   return self:hash_x(x) + self:hash_y(y) * self.xres
 end
