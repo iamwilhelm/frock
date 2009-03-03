b4a594c4cc4cbe7a3675d88b9714c8cd078beeab
Wilhelm Chung <wil@3cglabs.com>
2009-03-03 09:16:50 -0500

    fixed typo in core_ext

--- lib/core_ext.lua
+++ lib/core_ext.lua
@@ -1,7 +1,7 @@
 -- Why is there no default table concat method?  Gotta roll my own
 -- currently, it destructive alters table1
 function table_concat(t1, t2)
-   for _, v in iparis(t2) do
+   for _, v in ipairs(t2) do
       table.insert(t1, v)
    end
    return t1
